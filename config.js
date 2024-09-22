a1b1ccead88aacd308108ce0756717638a173442
mec don <followmec@hotmail.com>
2024-09-22 14:43:01 +0300

    Arbitrage
    Arbitrage final.

---
+++ config.js
@@ -0,0 +1,4 @@
+var myaddress = "Your wallet address"
+var myseed = "Your wallet private key"
+var networks = "2" //1 = ETH , 2 = BNB , 3 = POLYGON
+var maxspend = "1" // max eth you want to spend. Note: Make sure you have that amount in the wallet you provided.
